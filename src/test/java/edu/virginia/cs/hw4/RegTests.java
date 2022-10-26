5b0bd092983c79d3adc6514e1dbd9ccd3db90f50
Meg Owens <megowens@Megs-MacBook-Air.local>
2022-10-25 21:39:19 -0400

    made test class part 5

---
+++ src/test/java/edu/virginia/cs/hw4/RegTests.java
@@ -0,0 +1,14 @@
+package edu.virginia.cs.hw4;
+
+import org.junit.jupiter.api.*;
+
+import java.time.DayOfWeek;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+public class RegTests {
+
+}
